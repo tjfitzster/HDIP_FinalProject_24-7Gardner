d96ebb3beb6fc467dcce7dd20da1807dbe49b246
tjfitzzster <tjfitzster@gmail.com>
2022-03-20 16:39:23 +0000

    got the uid working for documents.

---
+++ Programs/Proof of Concept Programs/Android/IOTApplication/app/src/main/java/com/hdipin/computer/science/iotapplication/views/ForgotPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.hdipin.computer.science.iotapplication.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hdipin.computer.science.iotapplication.R
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
